7daa7e74f53ff184b86e6e113e5332bfbefc4ddb
jusep1993 <joseantoniolopezbenitez93@gmail.com>
2022-05-12 18:20:54 +0200

    Add lizard and Spockto game

--- script.js
+++ script.js
@@ -37,25 +37,69 @@ function roundWinner(player, computer){
     if(player == computer){
         console.log("It's a Tie!");
     } else if (player == "rock" && computer == "paper"){
-        console.log("Computer Wins the round!");
+        console.log("Paper covers Rock. Computer Wins the round!");
         computerScore++;
     }else if (player == "scissors" && computer == "paper"){
-        console.log("Player Wins the round!");
+        console.log("Scissors cuts Paper. Player Wins the round!");
         playerScore++;
+    }else if (player == "lizard" && computer == "paper"){
+        console.log("Lizard eats Paper. Player Wins the round!");
+        computerScore++;
+    }else if (player == "spock" && computer == "paper"){
+        console.log("Paper disproves Spock. Computer Wins the round!");
+        computerScore++;
     }else if (player == "paper" && computer == "rock"){
-        console.log("Player Wins the round!");
+        console.log("Paper covers Rock. Player Wins the round!");
         playerScore++;
     }else if (player == "scissors" && computer == "rock"){
-        console.log("Computer Wins the round!");
+        console.log("Rock crushes Scissors. Computer Wins the round!");
+        computerScore++;
+    }else if (player == "lizard" && computer == "rock"){
+        console.log("Rock crushes Lizard. Computer Wins the round!");
+        computerScore++;
+    }else if (player == "spock" && computer == "rock"){
+        console.log("Spock vaporizes Rock. Player Wins the round!");
         computerScore++;
     }else if (player == "rock" && computer == "scissors"){
-        console.log("Player Wins the round!");
+        console.log("Rock crushes Scissors. Player Wins the round!");
         playerScore++;
     }else if (player == "paper" && computer == "scissors"){
-        console.log("Computer Wins the round!");
+        console.log("Scissors cuts Paper. Computer Wins the round!");
+        computerScore++;
+    }else if (player == "lizard" && computer == "scissors"){
+        console.log("Scissors decapitates Lizard. Computer Wins the round!");
+        computerScore++;
+    }else if (player == "spock" && computer == "scissors"){
+        console.log("Spock smashes Scissors. Player Wins the round!");
+        computerScore++;
+    }else if (player == "rock" && computer == "lizard"){
+        console.log("Rock crushes Lizard. Player Wins the round!");
+        playerScore++;
+    }else if (player == "paper" && computer == "lizard"){
+        console.log("Lizard eats Paper. Computer Wins the round!");
+        computerScore++;
+    }else if (player == "spock" && computer == "lizard"){
+        console.log("Lizard poisons Spock. Computer Wins the round!");
+        computerScore++;
+    }else if (player == "scissors" && computer == "lizard"){
+        console.log("Scissors decapitates Lizard. Player Wins the round!");
+        playerScore++;
+    }else if (player == "lizard" && computer == "spock"){
+        console.log("Lizard poisons Spock. Player Wins the round!");
+        playerScore++;
+    }else if (player == "paper" && computer == "spock"){
+        console.log("Paper disproves Spock. Player Wins the round!");
+        playerScore++;
+    }else if (player == "rock" && computer == "spock"){
+        console.log("Spock vaporizes Rock. Computer Wins the round!");
+        computerScore++;
+    }else if (player == "scissors" && computer == "spock"){
+        console.log("Spock smashes Scissors. Computer Wins the round!");
         computerScore++;
     }
 }
+
+// Function to determine who wins the game
 
 function Score(player, computer){
 
